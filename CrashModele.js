0489dd084486d5cfa443c362c0d0221fecb781d0
alex-isitech <44968453+alex-isitech@users.noreply.github.com>
2018-11-13 11:02:27 +0100

    Add files via upload

---
+++ CrashModele.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose')
+​
+const CrashSchema = new mongoose.Schema({
+  bet: {
+    type: String,
+    required: true
+  },
+  player: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: String,
+    required: true
+  },
+})
+​
+CrashSchema.set('toJSON', {
+  transform: function (doc, ret, options){
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
+  }
+});
